a55a95d0d771fbbfc603e53db0f6a2a061b74f0b
Nama21yo <mataniayohanes@gmail.com>
2025-07-13 22:56:58 +0300

    feat: Add dbt tests and generate documentation

---
+++ dbt_project/models/marts/fct_messages.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table', schema='marts', unique_key='message_key') }}
+
+SELECT
+    CONCAT(s.message_id, '_', s.channel_name) AS message_key,
+    c.channel_id,
+    d.date_id,
+    s.message_timestamp,
+    s.message_text,
+    LENGTH(s.message_text) AS message_length,
+    s.has_image,
+    s.image_path
+FROM {{ ref('stg_telegram_messages') }} s
+JOIN {{ ref('dim_channels') }} c ON s.channel_name = c.channel_name
+JOIN {{ ref('dim_dates') }} d ON DATE(s.message_timestamp) = d.date
